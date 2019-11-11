f2efc6b7bafc08ccdfafec5dc5459957efeb46a8
DESKTOP-CJ5TJR4\MASENAK <peter736@uwm.edu>
2019-11-11 08:07:03 -0600

    init commit

---
+++ DCC-AlertBeacon/ALERTBEACON/ALERTBEACON/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ALERTBEACON
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
